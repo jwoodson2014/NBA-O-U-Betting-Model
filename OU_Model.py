f68acaedb76783e40cf9a6b3005558ecc95abe5f
Jason Woodson <90068313+jwoodson2014@users.noreply.github.com>
2022-01-22 11:18:12 -0600

    Add files via upload

--- OU_Model.py
+++ OU_Model.py
@@ -1,11 +1,12 @@
 from nba_api.stats.endpoints._base import Endpoint
 from nba_api.stats.library.http import NBAStatsHTTP
 from nba_api.stats.library.parameters import LastNGames, MeasureTypeDetailedDefense, Month, PaceAdjust, PerModeDetailed, Period, PlusMinus, Rank, Season, SeasonTypeAllStar, ConferenceNullable, DivisionSimpleNullable, GameScopeSimpleNullable, GameSegmentNullable, LeagueIDNullable, LocationNullable, OutcomeNullable, PlayerExperienceNullable, PlayerPositionAbbreviationNullable, SeasonSegmentNullable, ShotClockRangeNullable, StarterBenchNullable, DivisionNullable, DayOffset, GameDate, LeagueID
+from nba_api.stats.library.parameters import GameSegmentNullable, LastNGamesNullable, LeagueIDNullable, LocationNullable, MonthNullable, OutcomeNullable, PerModeSimpleNullable, PeriodNullable, SeasonNullable, SeasonSegmentNullable, SeasonTypeNullable, ShotClockRangeNullable, ConferenceNullable, DivisionNullable
 import datetime
+import random
 import pandas as pd
 import numpy as np
 import warnings
-
 warnings.filterwarnings('ignore')
 
 #scraping statistics from NBA.com
@@ -20,7 +21,7 @@ class LeagueDashTeamStats(Endpoint):
     headers = None
 
     def __init__(self,
-                 last_n_games=LastNGames.default,
+                 last_n_games=5,
                  measure_type_detailed_defense=MeasureTypeDetailedDefense.default,
                  month=Month.default,
                  opponent_team_id=0,
@@ -160,6 +161,89 @@ class Scoreboard(Endpoint):
         self.series_standings = Endpoint.DataSet(data=data_sets['SeriesStandings'])
         self.west_conf_standings_by_day = Endpoint.DataSet(data=data_sets['WestConfStandingsByDay'])
 
+#scraping the gamelogs from NBA.com
+class TeamGameLogs(Endpoint):
+    endpoint = 'teamgamelogs'
+    expected_data = {'TeamGameLogs': ['SEASON_YEAR', 'TEAM_ID', 'TEAM_ABBREVIATION', 'TEAM_NAME', 'GAME_ID', 'GAME_DATE', 'MATCHUP', 'WL', 'MIN', 'FGM', 'FGA', 'FG_PCT', 'FG3M', 'FG3A', 'FG3_PCT', 'FTM', 'FTA', 'FT_PCT', 'OREB', 'DREB', 'REB', 'AST', 'TOV', 'STL', 'BLK', 'BLKA', 'PF', 'PFD', 'PTS', 'PLUS_MINUS', 'GP_RANK', 'W_RANK', 'L_RANK', 'W_PCT_RANK', 'MIN_RANK', 'FGM_RANK', 'FGA_RANK', 'FG_PCT_RANK', 'FG3M_RANK', 'FG3A_RANK', 'FG3_PCT_RANK', 'FTM_RANK', 'FTA_RANK', 'FT_PCT_RANK', 'OREB_RANK', 'DREB_RANK', 'REB_RANK', 'AST_RANK', 'TOV_RANK', 'STL_RANK', 'BLK_RANK', 'BLKA_RANK', 'PF_RANK', 'PFD_RANK', 'PTS_RANK', 'PLUS_MINUS_RANK']}
+
+    nba_response = None
+    data_sets = None
+    player_stats = None
+    team_stats = None
+    headers = None
+
+    def __init__(self,
+                 date_from_nullable='',
+                 date_to_nullable='',
+                 game_segment_nullable=GameSegmentNullable.default,
+                 last_n_games_nullable=LastNGamesNullable.default,
+                 league_id_nullable=LeagueIDNullable.default,
+                 location_nullable=LocationNullable.default,
+                 measure_type_player_game_logs_nullable='Advanced',
+                 month_nullable=MonthNullable.default,
+                 opp_team_id_nullable=None,
+                 outcome_nullable=OutcomeNullable.default,
+                 po_round_nullable='',
+                 per_mode_simple_nullable=PerModeSimpleNullable.default,
+                 period_nullable=PeriodNullable.default,
+                 player_id_nullable='',
+                 season_nullable='2021-22',
+                 season_segment_nullable=SeasonSegmentNullable.default,
+                 season_type_nullable=SeasonTypeNullable.default,
+                 shot_clock_range_nullable=ShotClockRangeNullable.default,
+                 team_id_nullable='',
+                 vs_conference_nullable=ConferenceNullable.default,
+                 vs_division_nullable=DivisionNullable.default,
+                 proxy=None,
+                 headers=None,
+                 timeout=30,
+                 get_request=True):
+        self.proxy = proxy
+        if headers is not None:
+            self.headers = headers
+        self.timeout = timeout
+        self.parameters = {
+                'DateFrom': date_from_nullable,
+                'DateTo': date_to_nullable,
+                'GameSegment': game_segment_nullable,
+                'LastNGames': last_n_games_nullable,
+                'LeagueID': league_id_nullable,
+                'Location': location_nullable,
+                'MeasureType': measure_type_player_game_logs_nullable,
+                'Month': month_nullable,
+                'OppTeamID': opp_team_id_nullable,
+                'Outcome': outcome_nullable,
+                'PORound': po_round_nullable,
+                'PerMode': per_mode_simple_nullable,
+                'Period': period_nullable,
+                'PlayerID': player_id_nullable,
+                'Season': season_nullable,
+                'SeasonSegment': season_segment_nullable,
+                'SeasonType': season_type_nullable,
+                'ShotClockRange': shot_clock_range_nullable,
+                'TeamID': team_id_nullable,
+                'VsConference': vs_conference_nullable,
+                'VsDivision': vs_division_nullable
+        }
+        if get_request:
+            self.get_request()
+    
+    def get_request(self):
+        self.nba_response = NBAStatsHTTP().send_api_request(
+            endpoint=self.endpoint,
+            parameters=self.parameters,
+            proxy=self.proxy,
+            headers=self.headers,
+            timeout=self.timeout,
+        )
+        self.load_response()
+        
+    def load_response(self):
+        data_sets = self.nba_response.get_data_sets()
+        self.data_sets = [Endpoint.DataSet(data=data_set) for data_set_name, data_set in data_sets.items()]
+        self.team_game_logs = Endpoint.DataSet(data=data_sets['TeamGameLogs'])
+
+
 #saving the statistics to df's
 LeagueTraditional = LeagueDashTeamStats(measure_type_detailed_defense='Base',per_mode_detailed='Totals').get_data_frames()[0]
 LeagueAdvanced = LeagueDashTeamStats(measure_type_detailed_defense='Advanced',per_mode_detailed='PerGame').get_data_frames()[0]
@@ -195,6 +279,10 @@ OU_Model.to_excel(r'OU_Model.xlsx','Master Sheet')
 #creating function to calculate projected o/u for the day's games
 def get_todays_games(df1,df2):
     
+    print('Calculating projected totals....')
+    
+    random.seed(0)
+    
     #creating new df
     df3 = df1[['HOME_TEAM_ID','VISITOR_TEAM_ID']]
     
@@ -203,10 +291,6 @@ def get_todays_games(df1,df2):
     df3['Home Opp Pts Poss'] = ''
     df3['Visitor Pts Poss'] = ''
     df3['Visitor Opp Pts Poss'] = ''
-    df3['Home Pace'] = ''
-    df3['Visitor Pace'] = ''
-    df3['Proj Home Total'] = ''
-    df3['Proj Visitor Total'] = ''
     df3['Proj Total'] = ''
     df3['Fan Duel Total'] = ''
     df3['Decision'] = ''
@@ -220,7 +304,6 @@ def get_todays_games(df1,df2):
         team = df2.at[idx,'Team']
         pts = df2.at[idx,'PTS/POSS']
         opp_pts = df2.at[idx,'OPP PTS/POSS']
-        pace = df2.at[idx,'PACE']
         
         #iterate through each index position in the new df
         for idx in df3.index:
@@ -233,35 +316,45 @@ def get_todays_games(df1,df2):
                 df3.at[idx,'Home Team'] = team
                 df3.at[idx,'Home Pts Poss'] = pts
                 df3.at[idx,'Home Opp Pts Poss'] = opp_pts
-                df3.at[idx,'Home Pace'] = pace
             #if visitor_team_id is equal to the teamid at the index position in the ou_model
             elif item3 == item1:
                 df3.at[idx,'Visitor Team'] = team
                 df3.at[idx,'Visitor Pts Poss'] = pts
                 df3.at[idx,'Visitor Opp Pts Poss'] = opp_pts
-                df3.at[idx,'Visitor Pace'] = pace
             #if teamid is not found
             else:    
                 continue
     #iterate through each index position in the new df
     for idx in df3.index:
         
-        #calculate the max and min pace for teams in each matchup
-        max_pace = max(df3.at[idx,'Home Pace'],df3.at[idx,'Visitor Pace'])
-        min_pace = min(df3.at[idx,'Home Pace'],df3.at[idx,'Visitor Pace'])
+        #create an empty list to store results of simulated game total
+        gamescore = []
         
-        #calculate the proj home and visitor score based on their pts/poss, what the other team is allowing opponents, and the min and max pace of the matchup
-        proj_home_avg = ((max_pace * df3.at[idx,'Home Pts Poss']) + (min_pace * df3.at[idx,'Home Pts Poss']))/2
-        proj_home_opp_avg = ((max_pace * df3.at[idx,'Visitor Opp Pts Poss']) + (min_pace * df3.at[idx,'Visitor Opp Pts Poss']))/2
-        proj_visitor_avg = ((max_pace * df3.at[idx,'Visitor Pts Poss']) + (min_pace * df3.at[idx,'Visitor Pts Poss']))/2
-        proj_visitor_opp_avg = ((max_pace * df3.at[idx,'Home Opp Pts Poss']) + (min_pace * df3.at[idx,'Home Opp Pts Poss']))/2
+        #define the numnber of simulations you want to run
+        ns = 1000
         
-        #calculate the proj home and vistor total based on the calculations above
-        df3.at[idx,'Proj Home Total'] = (proj_home_avg + proj_home_opp_avg)/2
-        df3.at[idx,'Proj Visitor Total'] = (proj_visitor_avg + proj_visitor_opp_avg)/2
+        #scrape the game logs for the home and away team from NBA.com
+        home_poss = TeamGameLogs(team_id_nullable=df3.at[idx,'HOME_TEAM_ID']).get_data_frames()[0]
+        vis_poss = TeamGameLogs(team_id_nullable=df3.at[idx,'VISITOR_TEAM_ID']).get_data_frames()[0]
         
-        #calculate the proj total of the game
-        df3['Proj Total'] = df3['Proj Home Total'] + df3['Proj Visitor Total']
+        #calcuate the mean and standard deviation of possesions for both home and away in their previous 5 games
+        home_poss_mean = home_poss.iloc[:5]['POSS'].mean()
+        home_poss_std = home_poss.iloc[:5]['POSS'].std()
+        vis_poss_mean = vis_poss.iloc[:5]['POSS'].mean()
+        vis_poss_std = vis_poss.iloc[:5]['POSS'].std()                                                                                            
+        
+        
+        #for each simulation randomly calculate home and away possesions and take the average of those two numbers
+        #calcuate the score based on the number of possesions and save the score to the gamescore list                                                                                             
+        for i in range(ns):                                                                                             
+            poss = (random.gauss(home_poss_mean,home_poss_std) + random.gauss(vis_poss_mean,vis_poss_std))/2
+            hs = poss * ((df3.at[idx,'Home Pts Poss'] + df3.at[idx,'Visitor Opp Pts Poss'])/2)
+            aws = poss * ((df3.at[idx,'Visitor Pts Poss'] + df3.at[idx,'Home Opp Pts Poss'])/2)
+            score = hs + aws
+            gamescore.append(score)
+        
+        #save the average of the 1000 gamescores calculated to the Proj Total column
+        df3.at[idx,'Proj Total'] = (sum(gamescore)/len(gamescore))
     
     #iterate through each index position of the new df
     for idx in df3.index:
